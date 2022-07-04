73a623c5e1bd0b4c129623a0ea68c122be215d72
Falko Döhring <github@falko.in>
2022-07-04 22:37:04 +0200

    Adds total distance

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from tarfile import PAX_FIELDS
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import numpy as np
 
 st.set_page_config(page_title="Workout Dashboard",
                    page_icon=":running:",
                    layout="wide")
 df = pd.read_csv("allWorkouts.csv")
 
-st.dataframe(df)
+# st.dataframe(df)
 
 st.sidebar.header("Filter:")
 workoutTypes = st.sidebar.multiselect(
@@ -16,3 +17,20 @@ workoutTypes = st.sidebar.multiselect(
     options=pd.Series(df["Type"]).unique(),
     default=pd.Series(df["Type"]).unique()
 )
+
+df_selection = df.query(
+    "Type == @workoutTypes"
+)
+
+st.dataframe(df_selection)
+
+dfDistance = df_selection["Distance"].dropna()
+dfDistance = dfDistance.apply(lambda x: float(x.replace(",",".")))
+totalDistance = round(dfDistance.sum(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+
+with left_column:
+    st.subheader("Total Distance:")
+    st.subheader(f"{totalDistance:.2f} km")
+
